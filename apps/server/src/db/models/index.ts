8aef0494d45a63528cf0946691ad76ab46e39c7d
Анастасия <220038819+Nastasya-Mud@users.noreply.github.com>
2025-10-29 11:14:58 +0300

    6

--- apps/server/src/db/models/index.ts
+++ apps/server/src/db/models/index.ts
@@ -1,7 +1,13 @@
-export { User, IUser } from './User.js';
-export { Course, ICourse } from './Course.js';
-export { Lesson, ILesson } from './Lesson.js';
-export { PromoCode, IPromoCode } from './PromoCode.js';
-export { Activation, IActivation } from './Activation.js';
-export { Review, IReview } from './Review.js';
+export { User } from './User.js';
+export type { IUser } from './User.js';
+export { Course } from './Course.js';
+export type { ICourse } from './Course.js';
+export { Lesson } from './Lesson.js';
+export type { ILesson } from './Lesson.js';
+export { PromoCode } from './PromoCode.js';
+export type { IPromoCode } from './PromoCode.js';
+export { Activation } from './Activation.js';
+export type { IActivation } from './Activation.js';
+export { Review } from './Review.js';
+export type { IReview } from './Review.js';
 
